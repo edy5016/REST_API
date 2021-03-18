c984845ffb6349b828e4b20b391be591a0f6e8df
DongWookLee <edy5016@nbware.co.kr>
2021-03-18 12:03:15 +0900

    project creation

---
+++ src/main/java/com/rest/api/controller/EventController.java
@@ -0,0 +1,14 @@
+package com.rest.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@RestController
+@RequestMapping(value = "/api/events")
+@Slf4j
+public class EventController {
+
+}
